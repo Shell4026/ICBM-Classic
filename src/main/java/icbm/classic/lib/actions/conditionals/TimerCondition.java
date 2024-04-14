bf0cf4993610c061d2130486b7446a09816bf48d
Robin Seifert <rseifert.phone@gmail.com>
2024-04-14 14:00:20 -0400

    chore: changed timer condition to extend prefab

--- src/main/java/icbm/classic/lib/actions/conditionals/TimerCondition.java
+++ src/main/java/icbm/classic/lib/actions/conditionals/TimerCondition.java
@@ -1,11 +1,17 @@
 package icbm.classic.lib.actions.conditionals;
 
 import icbm.classic.ICBMConstants;
+import icbm.classic.api.actions.cause.IActionCause;
 import icbm.classic.api.actions.conditions.ICondition;
+import icbm.classic.api.actions.conditions.IConditionCause;
 import icbm.classic.api.actions.status.IActionStatus;
 import icbm.classic.lib.actions.status.ActionResponses;
+import icbm.classic.lib.saving.NbtSaveHandler;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagInt;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.INBTSerializable;
@@ -14,9 +20,10 @@ import javax.annotation.Nonnull;
 
 @NoArgsConstructor
 @AllArgsConstructor
-public class TimerCondition implements ICondition, INBTSerializable<NBTTagInt> {
+public class TimerCondition extends Condition implements IConditionCause {
     public static final ResourceLocation REG_NAME = new ResourceLocation(ICBMConstants.DOMAIN, "timer");
 
+    @Getter @Setter
     private int tick = 0;
 
     @Override
@@ -37,12 +44,23 @@ public class TimerCondition implements ICondition, INBTSerializable<NBTTagInt> {
     }
 
     @Override
-    public NBTTagInt serializeNBT() {
-        return new NBTTagInt(tick);
+    public NBTTagCompound serializeNBT() {
+        return SAVE_LOGIC.save(this, super.serializeNBT());
     }
 
     @Override
-    public void deserializeNBT(NBTTagInt nbt) {
-        tick = nbt.getInt();
+    public void deserializeNBT(NBTTagCompound nbt) {
+        super.deserializeNBT(nbt);
+        SAVE_LOGIC.load(this, nbt);
+    }
+
+    private static final NbtSaveHandler<TimerCondition> SAVE_LOGIC = new NbtSaveHandler<TimerCondition>()
+        .mainRoot()
+        /* */.nodeInteger("timer", TimerCondition::getTick, TimerCondition::setTick)
+        .base();
+
+    @Override
+    public IActionCause getCause() {
+        return null;
     }
 }
