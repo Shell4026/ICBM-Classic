1301eae82698b77e70c7ec69dd74e4c6684410c1
Robin Seifert <rseifert.phone@gmail.com>
2024-05-14 22:20:44 -0400

    feat: added jei sub-type handling and output logic for parachute/balloon

--- src/main/java/icbm/classic/mods/jei/CargoItemWrapper.java
+++ src/main/java/icbm/classic/mods/jei/CargoItemWrapper.java
@@ -1,7 +1,16 @@
 package icbm.classic.mods.jei;
 
+import icbm.classic.api.ICBMClassicAPI;
+import icbm.classic.api.missiles.projectile.IProjectileData;
+import icbm.classic.api.missiles.projectile.IProjectileStack;
+import icbm.classic.content.cargo.CargoProjectileData;
+import icbm.classic.content.cargo.balloon.BalloonProjectileData;
+import icbm.classic.content.cargo.parachute.ParachuteProjectileData;
+import icbm.classic.content.reg.ItemReg;
+import icbm.classic.lib.projectile.ProjectileStack;
 import lombok.AllArgsConstructor;
 import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.BlankRecipeWrapper;
 import mezz.jei.api.recipe.wrapper.ICraftingRecipeWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -10,19 +19,34 @@ import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class CargoItemWrapper implements ICraftingRecipeWrapper {
 
     private final ItemStack output;
-    private final List<ItemStack> options;
+    private final ItemStack input;
     private final ResourceLocation regName;
 
     @Override
     public void getIngredients(IIngredients iIngredients) {
 
-        iIngredients.setInputLists(ItemStack.class, Arrays.asList(Collections.singletonList(output), options));
-        iIngredients.setOutput(ItemStack.class, output);
+        iIngredients.setInputs(ItemStack.class, Arrays.asList(output, input));
+
+        final ItemStack out = output.copy();
+        final IProjectileStack projectileStack = out.getCapability(ICBMClassicAPI.PROJECTILE_STACK_CAPABILITY, null);
+        if(projectileStack instanceof ProjectileStack) {
+            CargoProjectileData data;
+            if(output.getItem() == ItemReg.itemBalloon) {
+                data = new BalloonProjectileData();
+            }
+            else {
+                data = new ParachuteProjectileData();
+            }
+            ((ProjectileStack) projectileStack).setProjectileData(data);
+            ((CargoProjectileData<?, ?>) data).setHeldItem(input.copy());
+        }
+        iIngredients.setOutput(ItemStack.class, Collections.singletonList(out));
     }
 
     @Override
--- src/main/java/icbm/classic/mods/jei/JEIProxy.java
+++ src/main/java/icbm/classic/mods/jei/JEIProxy.java
@@ -1,9 +1,14 @@
 package icbm.classic.mods.jei;
 
 import icbm.classic.ICBMConstants;
+import icbm.classic.api.ICBMClassicAPI;
+import icbm.classic.api.missiles.projectile.IProjectileStack;
+import icbm.classic.content.cargo.CargoProjectileData;
 import icbm.classic.content.reg.ItemReg;
+import icbm.classic.lib.projectile.ProjectileStack;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
 import net.minecraft.item.ItemStack;
@@ -12,6 +17,7 @@ import net.minecraft.util.ResourceLocation;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @JEIPlugin
 public class JEIProxy implements IModPlugin {
@@ -20,13 +26,37 @@ public class JEIProxy implements IModPlugin {
     // https://github.com/mekanism/Mekanism/tree/1.12/src/main/java/mekanism/client/jei
     // https://github.com/mezz/JustEnoughItems/wiki/Getting-Started
 
+    private String cargoItemKey(ISubtypeRegistry subtypeRegistry, ItemStack itemStack) {
+        String key = Integer.toString(itemStack.getMetadata());
+        if(itemStack.hasCapability(ICBMClassicAPI.PROJECTILE_STACK_CAPABILITY, null)) {
+            final IProjectileStack projectileStack = itemStack.getCapability(ICBMClassicAPI.PROJECTILE_STACK_CAPABILITY, null);
+            if (projectileStack instanceof ProjectileStack && projectileStack.getProjectileData() instanceof CargoProjectileData) {
+                final ItemStack stack = ((CargoProjectileData<?, ?>) projectileStack.getProjectileData()).getHeldItem();
+                if(stack != null && !stack.isEmpty()) {
+                    return key
+                        + ":" + stack.getItem().getRegistryName()
+                        + ":" + Optional.ofNullable(subtypeRegistry.getSubtypeInfo(stack)).orElse(Integer.toString(stack.getMetadata()));
+                }
+            }
+        }
+        return key;
+    }
+
+    @Override
+    public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
+        subtypeRegistry.registerSubtypeInterpreter(ItemReg.itemParachute, (i) -> this.cargoItemKey(subtypeRegistry, i));
+        subtypeRegistry.registerSubtypeInterpreter(ItemReg.itemBalloon, (i) -> this.cargoItemKey(subtypeRegistry, i));
+    }
+
     @Override
     public void register(IModRegistry registry) {
         //https://github.com/ExtraCells/ExtraCells2/blob/b1d03907d911909aaa3b176a7cc90eac0c1467dc/src/main/scala/extracells/integration/jei/Plugin.scala
 
         final List recipes = new ArrayList();
-        recipes.add(new CargoItemWrapper(new ItemStack(ItemReg.itemBalloon), registry.getIngredientRegistry().getIngredients(ItemStack.class), new ResourceLocation(ICBMConstants.DOMAIN, "balloon_cargo")));
-        recipes.add(new CargoItemWrapper(new ItemStack(ItemReg.itemParachute), registry.getIngredientRegistry().getIngredients(ItemStack.class), new ResourceLocation(ICBMConstants.DOMAIN, "parachute_cargo")));
+        for(ItemStack stack : registry.getIngredientRegistry().getIngredients(ItemStack.class)) {
+            recipes.add(new CargoItemWrapper(new ItemStack(ItemReg.itemBalloon), stack, new ResourceLocation(ICBMConstants.DOMAIN, "balloon_cargo")));
+            recipes.add(new CargoItemWrapper(new ItemStack(ItemReg.itemParachute), stack, new ResourceLocation(ICBMConstants.DOMAIN, "parachute_cargo")));
+        }
         //TODO cluster
         registry.addRecipes(recipes, VanillaRecipeCategoryUid.CRAFTING);
     }
